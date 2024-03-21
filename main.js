aa5244781363c8dbea19b06f2929d146455271ea
Hudson-Moreno <162583714+Hudson-Moreno@users.noreply.github.com>
2024-03-21 09:09:14 -0300

    just fixed the testing the shit

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let number = prompt('Bota um número ae!');
 function cry(number){
   alert('Hell yeah')
   if (number >= 9) {
-    console.log('Idk just doin shit)
+    console.log('Idk just doin shit')
   }
-};
+}
 
 cry(number);
 
-console.log(number)
+console.log(number);
